b73b8dd6f8cb18c7c1717f4323be45decd920dc0
sadiaprova12 <sadiaprova51717@gmail.com>
2024-03-07 23:34:04 +0600

    first

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+
+      {/* Bootstrap button */}
+      <button type="button" className="btn btn-primary">Primary</button>
+      <button type="button" className="btn btn-secondary">Secondary</button>
+      <button type="button" className="btn btn-success">Success</button>
+      <button type="button" className="btn btn-danger">Danger</button>
+      <button type="button" className="btn btn-warning">Warning</button>
+      <button type="button" className="btn btn-info">Info</button>
+      <button type="button" className="btn btn-light">Light</button>
+      <button type="button" className="btn btn-dark">Dark</button>
+      <button type="button" className="btn btn-link">Link</button>
+
+      {/* Bootstrap card  */}
+      <div className="card-group">
+  <div className="card">
+    <img src="..." className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div className="card">
+    <img src="..." className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div className="card">
+    <img src="..." className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+{/* End card */}
+
+    </div>
+  )
+}
+
+export default App
